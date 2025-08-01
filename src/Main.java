fc1b34f8526b0113e338b43bd414034e530a9a4e
YuriyM <Mikchailov1988@mail.ru>
2025-08-01 21:38:53 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,33 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задача 1");
+        char clientOs = '1';
+        if (clientOs == '0')
+            System.out.println("Установите версию приложения для iOS по ссылке ");
+        if (clientOs == '1') {
+            System.out.println("Установите версию приложения для Android по ссылке ");
+        }
+
+        System.out.println("Задача 2");
+        int clientOs2 = 1;
+        int clientDeviceYear = 2015;
+        if (clientOs2 == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке ");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке ");
+            }
+        }
+            else if (clientOs2 == 1) {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке ");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке ");
+                }
+            }
+
+        System.out.println("Задача 3!");
+
     }
 }
